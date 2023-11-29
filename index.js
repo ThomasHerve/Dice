3c972b9a51d77a9eb8ca5f6358209d9075166268
Thomas HERVE <therve38@gmail.com>
2023-11-29 16:12:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,82 +1,84 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token, gmId } = require('/usr/src/bot/config.json');
+const { token, gmId } = require('./config.json');
+const ERT = "**";
+const ER = "||";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.once(Events.ClientReady, c => {
 	console.log(`Ready, connected as ${c.user.tag}`);
-});
+});	
+
+function LaunchDice(msg)
+{
+	var total = 0;
+	var txt = "";
+	for (var k = 0; k < msg.length; k++)
+	{
+		var fmsg = msg[k].split('+');
+		for (var i = 0; i < fmsg.length; i++)
+		{
+			if (k > 0 && i == 0)
+				txt += "- "
+			else if (i > 0)
+				txt += "+ ";
+			fmsg[i] = fmsg[i].trim().toUpperCase();
+			if (fmsg[i].includes('D'))
+			{
+				const dice = fmsg[i].split('D');
+				const dices = parseInt(dice[0]);
+				const faces = parseInt(dice[1]);
+				txt += "( ";
+				for (var j = 0; j < dices; j++)
+				{
+					const res = Math.floor(Math.random() * faces) + 1;
+					if (k > 0 && i == 0)
+					{
+						total -= res;
+						txt += res + " ";
+						if (j < dices - 1)
+							txt += "+ ";
+					}
+					else
+					{
+						total += res;
+						txt += res + " ";
+						if (j < dices - 1)
+							txt += "+ ";
+					}
+				}
+				txt += ") ";
+			}
+			else
+			{
+				if (k > 0 && i == 0)
+				{
+					txt += fmsg[i] + " ";
+					total -= parseInt(fmsg[i]);
+				}
+				else
+				{
+					txt += fmsg[i] + " ";
+					total += parseInt(fmsg[i]);
+				}
+			}
+		}
+	}
+	return (`${ERT}${total}${ERT}\n${ER}${txt}${ER}`);
+}
 
 client.on(Events.MessageCreate, async message => {
-	if (message.content.startsWith("/roll"))
-	{
-		var total = 0;
-		var txt = "";
-		var msg = message.content.substring(6).split('+');
-		for (var i = 0; i < msg.length; i++)
-		{
-			if (i > 0)
-				txt += "+ ";
-			msg[i] = msg[i].trim().toUpperCase();
-			if (msg[i].includes('D'))
-			{
-				const dice = msg[i].split('D');
-				const dices = parseInt(dice[0]);
-				const faces = parseInt(dice[1]);
-				txt += "( ";
-				for (var j = 0; j < dices; j++)
-				{
-					const res = Math.floor(Math.random() * faces) + 1;
-					total += res;
-					txt += res + " ";
-					if (j < dices - 1)
-						txt += "+ ";
-				}
-				txt += ") ";
-			}
-			else
-			{
-				txt += msg[i] + " ";
-				total += parseInt(msg[i]);
-			}
-		}
-		message.reply(total + "\n" + txt);
-	}
-	if (message.content.startsWith("/gmroll")) {
-		var total = 0;
-		var txt = "";
-		var msg = message.content.substring(8).split('+');
-		for (var i = 0; i < msg.length; i++)
-		{
-			if (i > 0)
-				txt += "+ ";
-			msg[i] = msg[i].trim().toUpperCase();
-			if (msg[i].includes('D'))
-			{
-				const dice = msg[i].split('D');
-				const dices = parseInt(dice[0]);
-				const faces = parseInt(dice[1]);
-				txt += "( ";
-				for (var j = 0; j < dices; j++)
-				{
-					const res = Math.floor(Math.random() * faces) + 1;
-					total += res;
-					txt += res + " ";
-					if (j < dices - 1)
-						txt += "+ ";
-				}
-				txt += ") ";
-			}
-			else
-			{
-				txt += msg[i] + " ";
-				total += parseInt(msg[i]);
-			}
-		}
+	if (message.content.startsWith("/r "))
+		message.reply(LaunchDice(message.content.substring(3).split('-')));
+
+	if (message.content.startsWith("/roll "))
+		message.reply(LaunchDice(message.content.substring(6).split('-')));
+
+	if (message.content.startsWith("/gmroll ")) {
 		client.users.fetch(gmId, false).then((user) => {
-			user.send(message.author.displayName + " rolled a " + total + "\n" + txt);
+			user.send(`${message.author.displayName} rolled a ${LaunchDice(message.content.substring(8).split('-'))}`);
 		});
 	}
 });
